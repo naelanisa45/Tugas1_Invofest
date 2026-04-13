025aec6c246c853ea100005b2e3ec1afff37e1f9
naelanisa45 <naelanisa45@gmail.com>
2026-04-13 11:43:22 +0700

    Invofest

---
+++ src/components/Button.tsx
@@ -0,0 +1,21 @@
+import type React from "react";
+
+interface buttonProps{
+    title: string; //title akan berubah-ubah tergantung dari fe nya
+    variant: string;
+    className: string;
+}
+
+//untuk membuat component di react 
+export const Button: React.FC<buttonProps> =({ title, variant, className = "" }) => {
+    const primary = "bg-red-900 p-4 text-white rounded-lg";
+    const outline = "border border-red-900 p-4 py-2 rounded-lg";
+
+    return (
+        <button className={variant == "primary" ? primary : outline}>
+        {title}
+        </button>
+    );
+};
+
+export default Button;
