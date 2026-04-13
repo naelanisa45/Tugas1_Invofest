3c989e1771db03152dae4e880845f705930d3b9c
naelanisa45 <naelanisa45@gmail.com>
2026-04-13 12:12:05 +0700

    memperbaiki error

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,16 +3,16 @@ import type React from "react";
 interface buttonProps{
     title: string; //title akan berubah-ubah tergantung dari fe nya
     variant: string;
-    className: string;
+    className?: string;
 }
 
 //untuk membuat component di react 
 export const Button: React.FC<buttonProps> =({ title, variant, className = "" }) => {
     const primary = "bg-red-900 p-4 text-white rounded-lg";
-    const outline = "border border-red-900 p-4 py-2 rounded-lg";
+    const outline = "border border-red-900 text-red-900 px-4 py-2 rounded-lg";
 
     return (
-        <button className={variant == "primary" ? primary : outline}>
+        <button className={`${variant === "primary" ? primary : outline} ${className}`}>
         {title}
         </button>
     );
