c85547c0212dec8b20c9c5eff0293ec0359d9229
naelanisa45 <naelanisa45@gmail.com>
2026-04-27 21:00:47 +0700

    fix button & cleanup

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,29 +1,23 @@
-import type React from "react";
+import React from "react";
 
 interface ButtonProps {
   title?: string;
-  variant?: "primary" | "outline"; // ✅
   type?: "button" | "submit" | "reset";
   className?: string;
   children?: React.ReactNode;
 }
 
-//untuk membuat component di react 
-export const Button: React.FC<ButtonProps> =({ 
-    title, 
-    type = "button",
-    variant = "primary",
-    className, 
-    children,
+const Button: React.FC<ButtonProps> = ({
+  title,
+  type = "button",
+  className,
+  children,
 }) => {
-    const primary = "bg-red-900 p-4 text-white rounded-lg";
-    const outline = "border border-red-900 text-red-900 px-4 py-2 rounded-lg";
-
-    return (
-        <button type={type} className={className}>
-  {children || title}
-</button>
-    );
+  return (
+    <button type={type} className={className}>
+      {children || title}
+    </button>
+  );
 };
 
 export default Button;
